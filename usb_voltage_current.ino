c684e9e44e406067b4d51818c72fc6747d7e5c79
uribo <ur1b0.dev@gmail.com>
2020-09-15 20:28:23 +0900

    メイン処理実装

--- usb_voltage_current.ino
+++ usb_voltage_current.ino
@@ -1,9 +1,38 @@
+#define INA226_ADDR 0x40
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(10);
 
+  if( ina226_init( INA226_ADDR ) ){
+    while( 1 ) delay(10);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int16_t buff = 0;
+  byte result = 0;
 
+  result = ina226_read_mv( &buff );
+  Serial.print( "mv=" );
+  if( 0 == result ){
+    Serial.print( buff );
+  }
+
+  result = ina226_read_ma( &buff );
+    Serial.print( ";ma=" );
+  if( 0 == result ){
+    Serial.print( buff );
+  }
+
+  result = ina226_read_mw( &buff );
+  Serial.print( ";mw=" );
+  if( 0 == result ){
+    Serial.print( buff );
+  }
+  Serial.println( "" );
+
+  delay(1000);
 }
